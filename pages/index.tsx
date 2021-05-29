3366ab52f8e344fb5b04d1d1f83b9f4586ae21ba
homfarnam <homfarnam@gmail.com>
2021-05-29 23:43:35 +0430

    Fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import Layout from "../components/Layout";
 import { Client } from "@notionhq/client";
 
+export interface ProcessEnv {
+  [key: string]: string | undefined;
+}
+
 export default function IndexPage({ books }: any): JSX.Element {
   console.log("books: ", books);
   return (
@@ -41,7 +45,7 @@ export async function getStaticProps() {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
   const response = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID,
+    database_id: process.env.NOTION_DATABASE_ID!,
   });
 
   return {
